ffbc00bef70fe56cffd701cf27c7c507429d0964
James Swineson <github@public.swineson.me>
2019-03-20 16:00:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ BUILD_VERSION=${BUILD_VERSION:-"1.2.0"}
 DOCKER_IMAGE=${DOCKER_IMAGE:-"vyos-builder"}
 
 echo "configuring..."
-docker run --rm -it --privileged -v $(pwd):/vyos -w /vyos "${DOCKER_IMAGE}" ./configure --architecture amd64 --build-by "${BUILD_BY}" --build-type "${BUILD_TYPE}" --version "${BUILD_VERSION}"
+docker run --rm --privileged -v $(pwd):/vyos -w /vyos "${DOCKER_IMAGE}" ./configure --architecture amd64 --build-by "${BUILD_BY}" --build-type "${BUILD_TYPE}" --version "${BUILD_VERSION}"
 
 for var in "$@"
 do
     echo "Building $var..."
-    docker run --rm -it --privileged -v $(pwd):/vyos -w /vyos "${DOCKER_IMAGE}" make -j "${var}"
+    docker run --rm --privileged -v $(pwd):/vyos -w /vyos "${DOCKER_IMAGE}" make -j "${var}"
 done
 
