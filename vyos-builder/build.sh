07182ccc4756d90cc9e225cb7b29294d7d29f774
James Swineson <github@public.swineson.me>
2019-03-21 10:00:06 +0800

    update directory structure

--- vyos-builder/build.sh
+++ vyos-builder/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-BUILD_IMAGE_NAME=${BUILD_IMAGE_NAME:-"vyos-builder"}
+IMAGE_NAME=${IMAGE_NAME:-"vyos-builder"}
 BUILD_SCRIPT_BRANCH=${BUILD_SCRIPT_BRANCH:-crux}
 
 if ! [ -x "$(command -v unzip)" ]; then
@@ -16,5 +16,5 @@ unzip build_script.zip
 
 # build environment
 pushd "vyos-build-${BUILD_SCRIPT_BRANCH}"
-docker build -t ${BUILD_IMAGE_NAME}:${BUILD_SCRIPT_BRANCH} docker
+docker build -t ${IMAGE_NAME}:${BUILD_SCRIPT_BRANCH} docker
 popd
--- vyos/build.sh
+++ vyos/build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 BUILD_BY=${BUILD_BY:-"github@public.swineson.me"}
 BUILD_TYPE=${BUILD_TYPE:-"release"}
 BUILD_VERSION=${BUILD_VERSION:-"1.2.0"}
-DOCKER_IMAGE=${DOCKER_IMAGE:-"jamesits/vyos-builder:crux"}
+IMAGE_NAME=${IMAGE_NAME:-"jamesits/vyos-builder"}
 BUILD_SCRIPT_BRANCH=${BUILD_SCRIPT_BRANCH:-crux}
 
 if [[ $EUID -ne 0 ]]; then
@@ -25,14 +25,14 @@ unzip build_script.zip
 # build image
 pushd "vyos-build-${BUILD_SCRIPT_BRANCH}"
 echo "configuring..."
-docker run --rm --privileged -v $(pwd):/vyos -w /vyos "${DOCKER_IMAGE}" ./configure --architecture amd64 --build-by "${BUILD_BY}" --build-type "${BUILD_TYPE}" --version "${BUILD_VERSION}"
+docker run --rm --privileged -v $(pwd):/vyos -w /vyos "${DOCKER_IMAGE}:${BUILD_SCRIPT_BRANCH}" ./configure --architecture amd64 --build-by "${BUILD_BY}" --build-type "${BUILD_TYPE}" --version "${BUILD_VERSION}"
 
 for var in "$@"
 do
     echo "Building $var..."
     
     # do not set -j
-    docker run --rm --privileged -v $(pwd):/vyos -w /vyos "${DOCKER_IMAGE}" make "${var}"
+    docker run --rm --privileged -v $(pwd):/vyos -w /vyos "${DOCKER_IMAGE}:${BUILD_SCRIPT_BRANCH}" make "${var}"
 done
 
 # collect artifacts
