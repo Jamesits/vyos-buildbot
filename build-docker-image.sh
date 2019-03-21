a275494d5d376514ae0f4acc6aa201c23b90b45b
James Swineson <github@public.swineson.me>
2019-03-21 08:35:33 +0800

    Update build-docker-image.sh

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -4,10 +4,13 @@ set -euo pipefail
 BUILD_IMAGE_NAME=${BUILD_IMAGE_NAME:-"vyos-builder"}
 BUILD_SCRIPT_BRANCH=${BUILD_SCRIPT_BRANCH:-crux}
 
+if ! [ -x "$(command -v unzip)" ]; then
+  sudo apt-get install -y unzip
+fi
+
 # download scripts
 mkdir -p /tmp/vyos-build
 pushd /tmp/vyos-build
-sudo apt-get install -y unzip
 curl -L "https://github.com/vyos/vyos-build/archive/${BUILD_SCRIPT_BRANCH}.zip" -o build_script.zip
 unzip build_script.zip
 rm build_script.zip
