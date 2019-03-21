83b5402dc46cb0a53031590fd8e713d73f53fe45
James Swineson <github@public.swineson.me>
2019-03-21 08:34:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,10 +7,15 @@ BUILD_VERSION=${BUILD_VERSION:-"1.2.0"}
 DOCKER_IMAGE=${DOCKER_IMAGE:-"jamesits/vyos-builder:crux"}
 BUILD_SCRIPT_BRANCH=${BUILD_SCRIPT_BRANCH:-crux}
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 # download scripts
 mkdir -p /tmp/vyos-build
 pushd /tmp/vyos-build
-sudo apt-get install -y unzip
+apt-get install -y unzip
 curl -L "https://github.com/vyos/vyos-build/archive/${BUILD_SCRIPT_BRANCH}.zip" -o build_script.zip
 unzip build_script.zip
 rm build_script.zip
@@ -23,7 +28,7 @@ docker run --rm --privileged -v $(pwd):/vyos -w /vyos "${DOCKER_IMAGE}" ./config
 for var in "$@"
 do
     echo "Building $var..."
-    sudo docker run --rm --privileged -v $(pwd):/vyos -w /vyos "${DOCKER_IMAGE}" make -j "${var}"
+    docker run --rm --privileged -v $(pwd):/vyos -w /vyos "${DOCKER_IMAGE}" make -j "${var}"
 done
 
 cp build/* ${SYSTEM_ARTIFACTSTAGINGDIRECTORY}
